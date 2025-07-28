3776a9e04e2427422d3e82e464524966b71dfe23
sinahosseinzadeh97 <68941500+sinahosseinzadeh97@users.noreply.github.com>
2025-07-28 13:47:03 +0200

    Add files via upload

---
+++ Arrays_and_strings/RunningSumof1dArray.py
@@ -0,0 +1,27 @@
+'''Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+Return the running sum of nums.
+Example 1:
+Input: nums = [1,2,3,4]
+Output: [1,3,6,10]
+Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+]
+'''
+class Solution(object):
+    def runningSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        running = 0
+        result = []
+        for x in nums:
+            running +=x
+            result.append(running)
+        return result
+
+            
+
+nums = [1,2,3,4]
+sol = Solution()
+result = sol.runningSum(nums)
+print(result)
+        
